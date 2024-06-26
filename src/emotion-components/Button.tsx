e8e736b1584a2abcb669c73ac545ab4295575eed
joonmoogo <junemuk10@gmail.com>
2024-06-27 02:29:24 +0900

    second commit

---
+++ src/emotion-components/Button.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+export const Button = styled.div`
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    color: black;
+    font-weight: bold;
+    cursor:pointer;
+
+    &:hover {
+      color: white;
+    }
+`
---
+++ src/emotion-components/Layout.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+export const Layout = styled.div`
+    width:800px;
+    height:640px;
+    border: 1px solid black;
+    border-radius: 10px;
+`
